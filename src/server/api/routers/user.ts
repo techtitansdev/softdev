5b2a4d06014ab8a728467286157b1a5fa226e827
Yoshifumi Suzuki <yoshifumisuzuki81194@gmail.com>
2023-11-13 20:13:49 +0800

    feat: [Feature] created a db schema from the  ERD and  user api router with create public procedure

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,58 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure, } from "~/server/api/trpc";
+import { db } from "~/server/db";
+
+
+export const userRouter = createTRPCRouter({
+    /**
+     * 
+     *  const createNewUser = api.user.create.useMutation({
+        onError: error => {
+            console.log(error.name)
+        },
+        onSuccess: () => {
+            
+        }
+    });
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        createNewUser.mutate({ email, firstName, lastName, address, phone, password });
+    }
+     */
+    create: publicProcedure
+        .input(z.object({
+            email: z.string().toLowerCase(),
+            firstName: z.string(),
+            lastName: z.string(),
+            address: z.string(),
+            phone: z.string(),
+            password: z.string(),
+        }))
+        .mutation(async (opts) => {
+            const { input } = opts;
+
+            //check if email already exists in the database
+            const existingEmail = await db.user.findUnique({
+                where: { email: input.email }
+            });
+
+            if (existingEmail) {
+                throw new Error('Email is already in use');
+            }
+
+            //insert password hashing here
+            //const hashedPassword 
+
+            //add user credentials to the database
+            const user = await db.user.create({
+                data: {
+                    ...input,
+                    //password: hashedPassword,
+                },
+            });
+
+            return user;
+        }),
+
+})
