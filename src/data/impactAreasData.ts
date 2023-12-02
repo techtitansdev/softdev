c976c4cabb780a35ae7ba6b6990a6fc3e5ed77f8
Steven Ballaret <stevenballaret@gmail.com>
2023-12-02 09:32:40 +0800

    refactor(impact): fix impact areas data

--- src/data/impactAreasData.ts
+++ src/data/impactAreasData.ts
@@ -1,170 +1,47 @@
 export const impactAreasData = [
   {
-    impactName: "ArtsAndCulture",
-    title: "Arts & Culture",
+    impactName: "ReskillForTheFuture",
+    title: "Reskill For The Future",
     excerpt:
-      "Arts And culture are fundamental to a healthy society, helping people interpret the world and each other.",
+      "Education and Skills, Entrepreneurship, Workforce and Employment, Digital Transformation",
     image:
-      "https://i.pinimg.com/474x/72/4b/c2/724bc221ea0b516ebd065ddee37b10bd.jpg",
-    description:
-      "Arts and culture are key ingredients of a healthy society, helping people to interpret the world and each other. How we understand the world informs our decisions. As such, the place and role of artists and of arts and culture in society is critical.",
+      "rftf.png",
   },
   {
-    impactName: "CitiesAndUrbanization",
-    title: "Cities & Urbanization",
+    impactName: "BuildInclusiveCommunities",
+    title: "Build Inclusive Communites",
     excerpt:
-      "Smart and sustainable solutions enhance the liveability and efficiency of cities.",
+      "Gender parity, Accessibility and Disability, Social Justice, Racism, LGBTI Inclusion, Migration, Human Rights",
     image:
-      "https://i.pinimg.com/474x/fc/ad/cb/fcadcbe093c4409bfd5cd138041abe59.jpg",
-    description:
-      "Smart and sustainable solutions will need to be deployed to enhance the liveability, efficiency and productivity of cities. The emergence of digital infrastructure is inspiring innovative, cross-industry solutions to solve complex urban problems.",
+      "bic.png",
   },
   {
-    impactName: "CivicParticipation",
-    title: "Civic Participation",
+    impactName: "ProtectThePlanet",
+    title: "Protect The Planet",
     excerpt:
-      "For Shapers, collective action is central to influencing decision-making.",
+      "Climate Change, Sustainable Development, Biodiversity, Circular Economy, Plastics, Oceans, Water, Air Pollution",
     image:
-      "https://i.pinimg.com/474x/27/8c/23/278c2306334e0f6b62e6e9d7c2d59264.jpg",
-    description:
-      "Organized protests and popular unrest involving a diverse cross-section of society are taking place in all corners of the world. Increasingly, citizens are demanding change and are even influencing decision-making processes through this form of collective action.",
+      "ptp.png",
   },
   {
-    impactName: "ClimateChange",
-    title: "Climate Change",
-    excerpt: "Shapers are driving climate action in their local communities.",
+    impactName: "StrengthenCivicEngagement",
+    title: "Strengthen Civic Engagement",
+    excerpt: "Civic Participation, Justice",
     image:
-      "https://i.pinimg.com/474x/ae/07/88/ae0788f78306106d59443ca0b7686cfa.jpg",
-    description:
-      "Climate scientists have long highlighted the urgent need for accelerated climate action among all global stakeholders, including governments, businesses and civil society.",
+      "sce.png",
   },
   {
-    impactName: "COVID-19Response",
-    title: "COVID-19 Response",
-    excerpt:
-      "All stakeholders must urgently come together to minimize the impact of COVID-19.",
+    impactName: "DeliverBasicNeeds",
+    title: "Deliver Basic Needs",
+    excerpt: "Humanitarian Response.",
     image:
-      "https://media.istockphoto.com/id/1352251602/photo/healthcare-worker-giving-a-covid-19-vaccine-to-an-adult-man-at-the-hospital.webp?b=1&s=170667a&w=0&k=20&c=Gp6TqL0PWULAGEpuOi44aNI3EtSV_KPIt-qBs2d5BnE=",
-    description:
-      "The dramatic spread of COVID-19 has disrupted lives, livelihoods, communities and businesses worldwide. All stakeholders, especially young people, must urgently come together to minimize its impact on public health and limit its potential for further socioeconomic disruptions. Global Shapers are taking action by informing themselves and helping others.",
+      "dbn.png",
   },
   {
-    impactName: "Education",
-    title: "Education",
-    excerpt:
-      "In our increasingly knowledge-based society, Shapers are pushing innovation for education equality.",
+    impactName: "ImproveHealthAndWellness",
+    title: "Improve Health and Wellness",
+    excerpt: "Mental Health, Global Health, Ageing",
     image:
-      "https://i.pinimg.com/474x/e5/94/5c/e5945cc99bb9abb55fbcbb099e7d414a.jpg",
-    description:
-      "Education is a universal right and should not be for the privileged few. Although access to education has improved in recent years, more than 120 million young people are still out of school. As society becomes increasingly knowledge-based, innovative approaches are key to providing education for all.",
-  },
-  {
-    impactName: "Entrepreneurship",
-    title: "Entrepreneurship",
-    excerpt:
-      "Entrepreneurship is arguably the most fundamental driver of economic value creation in societies.",
-    image:
-      "https://i.pinimg.com/474x/7f/6e/0d/7f6e0dae66f4e50af7219a7cf645fe9e.jpg",
-    description:
-      "Entrepreneurs unleash the forces of creative destruction to transform industries and create new ones. Entrepreneurs are essential in bringing markets into competitive balance through their creative use of underutilized resources.",
-  },
-  {
-    impactName: "FourthIndustrialRevolution",
-    title: "Fourth Industrial Revolution",
-    excerpt:
-      "New technologies are merging the physical and digital worlds in ways that create both huge promise and potential peril.",
-    image:
-      "https://i.pinimg.com/474x/b0/17/ed/b017ed4e5584fc8d4419050f1d575540.jpg",
-    description:
-      "The Fourth Industrial Revolution represents a fundamental change in the way people live, work and relate to one another. The speed, breadth and depth of this revolution is forcing us to rethink how countries develop, how organizations create value and even what it means to be human.",
-  },
-  {
-    impactName: "Gender Equality",
-    title: "GenderEquality",
-    excerpt:
-      "Despite strong achievements, progress is still slow to realize the full potential of one half of humanity.",
-    image:
-      "https://i.pinimg.com/474x/08/d6/fc/08d6fc1f44d2c2144cf355eb15a17d2e.jpg",
-    description:
-      "There is a fundamental moral case for empowering women: women represent one-half of the global population and it is self-evident that they must have equal access to health, education, earning power and political representation.",
-  },
-  {
-    impactName: "GlobalHealth",
-    title: "Global Health",
-    excerpt:
-      "The future of global health requires integration of public- and private-sector innovations to address traditional and emerging challenges.",
-    image:
-      "https://i.pinimg.com/474x/57/af/d4/57afd48a1773d07386bd2e3ba2ac49f9.jpg",
-    description:
-      "The current global health agenda faces a trio of challenges that threatens socio-economic development: infectious diseases, the rise of non-communicable illnesses, and the strain on public health facilities resulting from rapid globalization and climate change.",
-  },
-  {
-    impactName: "Migration",
-    title: "Migration",
-    excerpt:
-      "Emigration and immigration provide states, societies and migrants with tremendous opportunities.",
-    image:
-      "https://i.pinimg.com/474x/3f/61/de/3f61de602f96c585796adbb5151737a7.jpg",
-    description:
-      "Migration has emerged in the last few years as a critical political and policy challenge for many states, and includes dimensions such as integration, social cohesion, the attraction and retention of skills and labour, as well as border management.",
-  },
-  {
-    impactName: "ShapingMyCity'sFuture",
-    title: "Shaping My City's Future",
-    excerpt:
-      "What will it take to build cities of the future in which everyone can live and thrive?",
-    image:
-      "https://i.pinimg.com/474x/0d/15/b1/0d15b1fcafb2b0a1e1b68b477c0e9eb7.jpg",
-    description:
-      "The #ShapingMyCitysFuture challenge asked young people around the world: What would it take to build cities of the future in which everyone – regardless of gender, race, income, age or ability – can live and thrive? In 2018, Shapers will bring their ideas to life in their hubs and communities.",
-  },
-  {
-    impactName: "SustainableDevelopment",
-    title: "Sustainable Development",
-    excerpt:
-      "Governments, organizations and individuals are addressing pressing global issues together through the SDGs.",
-    image:
-      "https://i.pinimg.com/474x/9d/3a/93/9d3a93bf1c2a0e5ce48fe1552df51ea4.jpg",
-    description:
-      "Governments, organizations and individuals are working together to address pressing global issues while ensuring the Sustainable Development Goals (SDGs) are universally applied. The SDGs are action-oriented, calling on all countries to take ownership of implementing solutions.",
-  },
-  {
-    impactName: "Values",
-    title: "Values",
-    excerpt:
-      "Some values are universal, transcending national boundaries and enhancing the collective human experience.",
-    image:
-      "https://i.pinimg.com/564x/cb/55/f5/cb55f5088aa0670f5eb942bb8d598e12.jpg",
-    description:
-      "Values serve as a pillar of a healthy society, ensuring that trust and integrity are foundations on which governments, organizations and individuals can establish prosperous and healthy relationships.",
-  },
-  {
-    impactName: "Water",
-    title: "Water",
-    excerpt:
-      "Shapers recognize and are addressing water scarcity as one of the fastest-growing social, political and economic challenges of our time.",
-    image:
-      "https://i.pinimg.com/474x/b5/50/45/b5504564ba91b87dbc2e3348c258980a.jpg",
-    description:
-      "Water crises are among the fastest-growing social, political and economic challenges, ranking as one of the greatest long-term risks in the world. Water scarcity is bound to become more and more pressing.",
-  },
-  {
-    impactName: "#WeSeeEqual",
-    title: "#WeSeeEqual",
-    excerpt: "Shapers are challenging the status quo on gender inequality.",
-    image:
-      "https://i.pinimg.com/474x/3c/24/bb/3c24bb464525e188d412c95fa406c277.jpg",
-    description:
-      "Following the Annual Meeting 2018, Shapers and Procter and Gamble formed a partnership to raise awareness on gender equality. As part of the campaign, Shapers sparked a social media conversation on how and why they are challenging the status quo. #WeSeeEqual has reached over a million people to date. Over 100 city-based hubs around the world submitted their grassroots solutions to gender inequality. Five winning hubs were awarded a 20,000 CHF grant challenge to bring their ideas to life. Explore their solutions below.",
-  },
-  {
-    impactName: "WorkforceAndEmployment",
-    title: "Workforce and Employment",
-    excerpt:
-      "Shapers are recognizing new work models and preparing young people for shifting job roles and skills demand.",
-    image:
-      "https://i.pinimg.com/474x/36/3b/9b/363b9b2b31e285b7be6b6542ba600635.jpg",
-    description:
-      "The world of work is changing faster than before, and mapping, as well as recognizing new work models, will be key to harnessing and directing that change towards stronger labour markets.",
+      "ihw.png",
   },
 ];
