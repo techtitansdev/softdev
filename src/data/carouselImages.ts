7220b3f718d22361b62ac8a69a221ce5f9c4bdfe
Steven Ballaret <stevenballaret@gmail.com>
2023-12-04 14:34:36 +0800

    refactor(homepage): fix carousel images

--- src/data/carouselImages.ts
+++ src/data/carouselImages.ts
@@ -1,6 +1,6 @@
 export const carouselImages = [
-  "carousel-4.jpg",
-  "carousel-6.jpg",
-  "carousel-5.jpg",
+  "carousel-1.jpg",
   "carousel-2.jpg",
+  "carousel-3.jpg",
+  "carousel-4.jpg",
 ];
