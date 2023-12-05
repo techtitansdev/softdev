3ee6bf18431b55409fde77ede99087553e8a4451
Steven Ballaret <stevenballaret@gmail.com>
2023-12-05 17:23:49 +0800

    refactor(homepage): fix carousel height

--- src/pages/home/Carousel.tsx
+++ src/pages/home/Carousel.tsx
@@ -29,7 +29,7 @@ export const Carousel = ({ slides }: CarouselProps) => {
   };
 
   return (
-    <div className="rounded-xl relative mx-auto mb-8 mt-8 max-h-[640px] min-w-[200px] max-w-[1240px] overflow-hidden">
+    <div className="relative mx-auto mb-8 mt-8 max-h-[600px] min-w-[200px] max-w-[1240px] overflow-hidden rounded-xl">
       <motion.div
         className={`duration-400 flex transition-transform ease-out`}
         style={{
