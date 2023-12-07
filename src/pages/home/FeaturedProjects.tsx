5c7792c878cc1216d696cced4b5492ba2329d482
Steven Ballaret <stevenballaret@gmail.com>
2023-12-07 12:32:14 +0800

    refactor(homepage): polish responsiveness and images in featured projects

--- src/pages/home/FeaturedProjects.tsx
+++ src/pages/home/FeaturedProjects.tsx
@@ -8,19 +8,22 @@ export const FeaturedProjects = () => {
   };
 
   return (
-    <div className="bg-gray-50 bg-cover">
-      <div className="mx-auto max-w-screen-xl py-2 pb-12 sm:p-10 lg:p-6 lg:py-6">
-        <div className="mt-4 text-center text-2xl font-medium text-gray-800 sm:text-3xl lg:text-left">
+    <div
+      className="bg-cover bg-center bg-gray-100"
+     
+    >
+      <div className="mx-auto max-w-screen-xl py-6 pb-10 lg:p-6 lg:py-8 lg:pb-12">
+        <div className="mx-auto mt-4 text-center text-2xl font-medium text-gray-800 sm:max-w-[610px] sm:text-left sm:text-3xl xl:mx-0 xl:max-w-[900px]">
           FEATURED PROJECTS
         </div>
 
-        <div className="px-6 pb-5 text-center text-sm font-normal text-gray-700 sm:px-0 md:text-base lg:text-left">
+        <div className="mx-auto px-4 pb-5 text-center text-sm font-normal text-gray-700 sm:max-w-[610px] sm:px-0 sm:text-left xl:mx-0 xl:max-w-[900px] xl:text-base">
           Shaping Iloilo through our new hub projects by implementing innovative
           solutions that foster positive social change.
         </div>
 
         <div className="mb-2 flex items-center justify-center">
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4">
             {featuredProjects.map((card, index) => (
               <div
                 key={index}
@@ -32,11 +35,10 @@ export const FeaturedProjects = () => {
                 className="w-full transform justify-self-start rounded-lg border border-gray-200 bg-white font-medium shadow transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 dark:bg-white"
               >
                 <img
-                  className="h-52 w-72 rounded-t-lg"
+                  className="h-52 w-72 rounded-t-lg object-cover"
                   src={card.image}
                   alt={card.title}
                 />
-
                 <div className="p-4">
                   <div className="card-title">
                     <div className="mb-1 text-xl tracking-tight text-gray-900">
