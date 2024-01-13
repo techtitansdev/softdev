ac8826454366ded0eb6d394cccfe792a401b3bdf
Steven Ballaret <stevenballaret@gmail.com>
2024-01-13 09:41:27 +0800

    feat(partners): added partner page

---
+++ src/data/partners.ts
@@ -0,0 +1,20 @@
+export const partners = [
+  {
+    image: "devcon.jpeg",
+  },
+  {
+    image: "innovate-iloilo.png",
+  },
+  {
+    image: "mulave.png",
+  },
+  {
+    image: "umwad.jpeg",
+  },
+  {
+    image: "gdap.png",
+  },
+  {
+    image: "iloilo-business-club.png",
+  },
+];
