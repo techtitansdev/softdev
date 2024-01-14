14c9c15a7a4ffdc3ab8dbb25851fedec1069a993
Steven Ballaret <stevenballaret@gmail.com>
2024-01-14 17:45:08 +0800

    refactor(coming-soon): added coming soon image

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -5,7 +5,7 @@ export const ComingSoon = () => {
         className="flex max-h-[800px] flex-col items-center bg-gray-50 xl:h-screen"
         style={{
           backgroundImage:
-            "url('https://i.pinimg.com/474x/f2/7c/23/f27c2356e1943df4875ed932707e9931.jpg')",
+            "url('/coming-soon.jpeg')",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
@@ -16,7 +16,7 @@ export const ComingSoon = () => {
               COMING SOON
             </div>
             <div className="max-w-[400px] px-2 text-center text-xl font-light sm:max-w-[600px] lg:max-w-[880px]">
-              Our new website is currently under construction and will soon be
+              Our website is currently under construction and will soon be
               your hub for impactful initiatives and collaborative projects. Get
               ready to be part of a global community making a difference!"
             </div>
