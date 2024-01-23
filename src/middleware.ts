b22e5104c436dffffea698d8331aeca4e852292e
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-01-23 19:02:43 +0800

    feat(tech4all): updated tech4all page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-    publicRoutes:['/','/register', "/api/auth/session",'/login','/home', '/about-us', '/impact']
+    publicRoutes:['/','/register', "/api/auth/session",'/login','/home', '/about-us', '/impact', '/partners', '/projects/tech4all']
 });
  
 export const config = {
