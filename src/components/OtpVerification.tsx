d8e432bd507b8e01b9b43409abc0479689fa4417
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-01-31 19:03:09 +0800

    fix(otp verification): fix the otp verification ui, where you can backspace it

--- src/components/OtpVerification.tsx
+++ src/components/OtpVerification.tsx
@@ -1,60 +1,68 @@
-
 import { useSignUp } from "@clerk/nextjs";
 import router from "next/router";
 import React, { useState } from "react";
 
- 
- export const OtpVerification = () => {
+export const OtpVerification = () => {
   const [otp, setOtp] = useState<string[]>(new Array(6).fill(""));
   const { isLoaded, signUp, setActive } = useSignUp();
   const [code, setCode] = useState("");
+
   const onPressVerify = async () => {
     if (!isLoaded) {
       return;
     }
-   
+
     try {
       const completeSignUp = await signUp.attemptEmailAddressVerification({
         code,
       });
       if (completeSignUp.status !== "complete") {
-        /* investigate the response, to see if there was an error
-         or if the user needs to complete more steps.*/
         console.log(JSON.stringify(completeSignUp, null, 2));
       }
       if (completeSignUp.status === "complete") {
-        await setActive({ session: completeSignUp.createdSessionId })
+        await setActive({ session: completeSignUp.createdSessionId });
         router.push("/");
       }
     } catch (err: any) {
       console.error(JSON.stringify(err, null, 2));
     }
-   };
-  
+  };
 
-  const handleChange = (el: EventTarget & HTMLInputElement, index: number) => {
-    const inputValue = el.value;
+  const handleChange = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    if (e.key === "Backspace") {
+      const newOtp = [...otp];
+      newOtp[index] = "";
+      setOtp(newOtp);
 
-    // Check if the input is a numeric digit (0-9) or an empty string
-    if (!/^\d*$/.test(inputValue)) return false;
+      // Move focus to the previous input field
+      if (index > 0) {
+        const prevInput = e.currentTarget.previousSibling as HTMLInputElement;
+        prevInput.focus();
+      }
+    } else {
+      const inputValue = e.key;
 
-    const newOtp = [...otp];
-    newOtp[index] = inputValue;
-    setOtp(newOtp);
+      // Check if the input is a numeric digit (0-9)
+      if (/^\d$/.test(inputValue)) {
+        const newOtp = [...otp];
+        newOtp[index] = inputValue;
+        setOtp(newOtp);
 
-    // Manage focus to the next input field
-    if (el.nextSibling && inputValue !== "" && index < otp.length - 1) {
-      const nextInput = el.nextSibling as HTMLInputElement;
-      nextInput.focus();
+        // Move focus to the next input field
+        if (index < otp.length - 1) {
+          const nextInput = e.currentTarget.nextSibling as HTMLInputElement;
+          nextInput.focus();
+        }
+      }
     }
   };
 
-  // Submit OTP handler
- 
-
   return (
-    <div className=" m-auto flex min-h-screen items-center justify-center">
-      <div className="md: mx-2 rounded-xl border-2 py-5 md:px-8">
+    <div className="m-auto flex min-h-screen items-center justify-center">
+      <div className="rounded-xl border-2 py-5 md:mx-2 md:px-8">
         <div className="text:xl mb-2 text-center font-semibold md:text-2xl">
           Verify your Email
         </div>
@@ -62,26 +70,24 @@ import React, { useState } from "react";
           Please enter the verification code we sent to your email
         </div>
         <div className="m-auto flex flex-row gap-1 sm:gap-2 md:gap-6">
-          {otp.map((data, i) => {
-            return (
-              <input
-                type="text"
-                name="otp"
-                className="m-auto h-12 w-12 border-b-2 border-gray-600 text-center text-2xl"
-                maxLength={1}
-                key={i}
-                value={data}
-                onChange={(e) => handleChange(e.target, i)}
-                onFocus={(e) => e.target.select()}
-              />
-            );
-          })}
+          {otp.map((data, i) => (
+            <input
+              type="text"
+              name="otp"
+              className="m-auto h-12 w-12 border-b-2 border-gray-600 text-center text-2xl"
+              maxLength={1}
+              key={i}
+              value={data}
+              onKeyDown={(e) => handleChange(e, i)}
+              onFocus={(e) => e.target.select()}
+            />
+          ))}
         </div>
         <div className="m-auto my-5 flex w-[80%] flex-row justify-center gap-2">
           <button
             onClick={() => {
               const enteredOtp = otp.join("");
-              console.log(otp.join(""))
+              console.log(otp.join(""));
               setCode(otp.join(""));
               setTimeout(() => {
                 onPressVerify();
