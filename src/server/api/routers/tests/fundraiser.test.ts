5a9b6d45ead8a50142681a70a01b7959fabed2c2
Yoshifumi Suzuki <yoshifumisuzuki81194@gmail.com>
2024-02-10 11:19:43 +0800

    test(fundraiser.test.ts): tests the getAll procedure of the fundraiser router

--- src/server/api/routers/tests/fundraiser.test.ts
+++ src/server/api/routers/tests/fundraiser.test.ts
@@ -62,5 +62,78 @@ describe("Fundraiser procedures testing", () => {
         throw error;
       }
     });
+    it("Should get all the fundraisers", async () => {
+      const projectInput = {
+        title: "Project 11111",
+        description: "Project Description",
+        image: "/tech4all.png",
+        hub: "Project Hub",
+        category: "Project Category",
+        type: "Project Type",
+        beneficiaries: "Beneficiaries",
+        about: "About Project",
+      };
+      try {
+        const project = await callerProject.create(projectInput);
+        const project2 = await callerProject.create(projectInput);
+
+        await ctx.db.fundraisers.deleteMany({});
+
+        const fundraiserInput = {
+          projectId: project.id,
+          funds: 127,
+          goal: 999,
+          targetDate: new Date("2024-02-08T05:47:17.947Z"),
+        };
+
+        const fundraiserInput2 = {
+          projectId: project2.id,
+          funds: 127,
+          goal: 999,
+          targetDate: new Date("2024-02-08T05:47:17.947Z"),
+        };
+        const fundraiser1 = await callerFundraiser.create(fundraiserInput);
+        const fundraiser2 = await callerFundraiser.create(fundraiserInput2);
+
+        const allFundraisers = await callerFundraiser.getAll();
+        expect(allFundraisers.length).toBe(2);
+
+        if (!allFundraisers) {
+          throw new Error("All fundraisers are undefined");
+        } else {
+          expect(allFundraisers[0]!.projectId).toBe(project.id);
+          expect(allFundraisers[0]!.funds).toBe(fundraiserInput.funds);
+          expect(allFundraisers[0]!.goal).toBe(fundraiserInput.goal);
+          expect(allFundraisers[0]!.targetDate).toStrictEqual(
+            fundraiserInput.targetDate,
+          );
+
+          expect(allFundraisers[0]!.projectId).toBe(fundraiser1.projectId);
+          expect(allFundraisers[0]!.funds).toBe(fundraiser1.funds);
+          expect(allFundraisers[0]!.goal).toBe(fundraiser1.goal);
+          expect(allFundraisers[0]!.targetDate).toStrictEqual(
+            fundraiser1.targetDate,
+          );
+
+          expect(allFundraisers[1]!.projectId).toBe(project2.id);
+          expect(allFundraisers[1]!.funds).toBe(fundraiserInput.funds);
+          expect(allFundraisers[1]!.goal).toBe(fundraiserInput.goal);
+          expect(allFundraisers[1]!.targetDate).toStrictEqual(
+            fundraiserInput.targetDate,
+          );
+
+          expect(allFundraisers[1]!.projectId).toBe(fundraiser2.projectId);
+          expect(allFundraisers[1]!.funds).toBe(fundraiser2.funds);
+          expect(allFundraisers[1]!.goal).toBe(fundraiser2.goal);
+          expect(allFundraisers[1]!.targetDate).toStrictEqual(
+            fundraiser2.targetDate,
+          );
+
+        }
+      } catch (error) {
+        console.error("Error during creation:", error);
+        throw error;
+      }
+    });
   });
 });
