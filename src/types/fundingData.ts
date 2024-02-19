fee3456398db63fb7e13fc1ca1f8b63cfb668853
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-02-19 12:05:29 +0800

    feat(create fundraiser): added partial ui for create fundraiser

---
+++ src/types/fundingData.ts
@@ -0,0 +1,13 @@
+export interface FundingData {
+  title: string,
+  description: string,
+  image: string,
+  hub: string,
+  category: string,
+  type: string,
+  beneficiaries: string,
+  milestones: string,
+  goal: number,
+  date: string,
+  about: string,
+}
