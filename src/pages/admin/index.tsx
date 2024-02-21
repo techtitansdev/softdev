6d45cd6f4a9af5c0d75eb3394f6c6c54a02424ac
jiv <jivayydelacruz@gmail.com>
2024-02-22 00:43:49 +0800

    fix: fix merge conflict issues

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -4,26 +4,25 @@ import React from "react";
 import { Sidebar } from "~/components/Sidebar";
 import { api } from "~/utils/api";
 
-const Admin = () => {
+export const Admin = () => {
   const { user, isLoaded } = useUser();
   const user_role = user?.publicMetadata.admin;
   const router = useRouter();
-  console.log(user_role)
+  console.log(user_role);
 
   useEffect(() => {
-    if (isLoaded && user_role !== 'admin') {
-      router.push('/home');
+    if (isLoaded && user_role !== "admin") {
+      router.push("/home");
     }
   }, [isLoaded, user_role]);
 
   if (!isLoaded) {
     return <div>Loading...</div>;
   }
-  if (isLoaded && user_role !== 'admin'){
+  if (isLoaded && user_role !== "admin") {
     return <div>UNAUTHORIZED</div>;
   }
 
-export const Admin = () => {
   return (
     <>
       <Head>
@@ -39,11 +38,12 @@ export const Admin = () => {
     </>
   );
 };
-  function useUser(): { user: any; isLoaded: any; } {
-    throw new Error("Function not implemented.");
-  }
+function useUser(): { user: any; isLoaded: any } {
+  throw new Error("Function not implemented.");
+}
 
-  function useEffect(arg0: () => void, arg1: any[]) {
-    throw new Error("Function not implemented.");
-  }
+function useEffect(arg0: () => void, arg1: any[]) {
+  throw new Error("Function not implemented.");
+}
 
+export default Admin;
