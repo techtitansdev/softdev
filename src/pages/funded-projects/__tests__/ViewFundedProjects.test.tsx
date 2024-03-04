05f1fa9310303d9b90cc51c65eea50017c18eff6
jiv <jivayydelacruz@gmail.com>
2024-03-04 18:23:27 +0800

    test(viewfundedprojects.test.tsx): added missing mock data

--- src/pages/funded-projects/__tests__/ViewFundedProjects.test.tsx
+++ src/pages/funded-projects/__tests__/ViewFundedProjects.test.tsx
@@ -3,6 +3,7 @@ import FilterByCategory from "~/components/FilterByCategory";
 import { Footer } from "~/components/Footer";
 import SearchInput from "~/components/SearchInput";
 import FundedProjects from "../index";
+import { Navbar } from "~/components/Navbar";
 
 jest.mock("next/router", () => ({
   useRouter: () => ({
@@ -10,13 +11,48 @@ jest.mock("next/router", () => ({
   }),
 }));
 
+jest.mock("@clerk/nextjs", () => ({
+  useUser: jest.fn().mockReturnValue({
+    user: {
+      publicMetadata: {
+        user: "user",
+      },
+    },
+    isLoaded: true,
+  }),
+  useClerk: jest.fn().mockReturnValue({
+    signOut: jest.fn(),
+  }),
+}));
+
+jest.mock("~/utils/api", () => ({
+  api: {
+    fundraiser: {
+      getAll: {
+        useQuery: jest.fn().mockReturnValue({
+          data: [
+            {
+              id: 1,
+              project: {
+                title: "Project Title",
+                category: "Category A",
+                published: true,
+              },
+            },
+          ],
+        }),
+      },
+    },
+  },
+}));
+
 describe("renders Funding Page view", () => {
-  test("renders FundedProjects view correctly", async () => {
-    const { findByText } = render(<FundedProjects />);
+  test("renders FundedProjects view correctly", () => {
+    render(<FundedProjects />);
+  });
 
-    const fundraiserPageContents = await findByText("Funded Projects");
-
-    expect(fundraiserPageContents).toBeTruthy();
+  test("renders Navbar components correctly", () => {
+    render(<Navbar />);
   });
 
   test("renders FilterByCategory component correctly", () => {
