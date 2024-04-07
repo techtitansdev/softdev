b8a1bc356ff872fc7286fe80e671e540823c4dd3
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-04-07 23:18:02 +0800

    feat(admin comment view): added comment ui with search,filter,and sort

---
+++ src/data/commentData.ts
@@ -0,0 +1,176 @@
+export const commentData = [
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Jiv",
+    date: "Feb 22, 2020",
+    comment: "There is a problem about this.",
+    projectName: "Project X",
+    email: "jiverlee@gmail.com",
+  },
+  {
+    name: "Yoshi",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Yoshi",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Yoshi",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Jiv",
+    date: "Feb 22, 2020",
+    comment: "There is a problem about this.",
+    projectName: "Project X",
+    email: "jiverlee@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project F",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project S",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Yoshi",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project D",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project D",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project D",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project A",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project B",
+    email: "ferjendave@gmail.com",
+  },
+  {
+    name: "Ferjen Dave",
+    date: "Jan 22, 2020",
+    comment:
+      "The project is really nice. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    projectName: "Project B",
+    email: "ferjendave@gmail.com",
+  },
+];
