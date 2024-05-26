a3f178f7bfbc37a7589214789a6cfa8b6cf61732
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-05-27 00:21:47 +0800

    fix(partners): fix the footer to be fixed at the bottom

--- src/pages/partners/index.tsx
+++ src/pages/partners/index.tsx
@@ -24,43 +24,53 @@ const Partners = () => {
         <link rel="icon" href="/gsi-logo.png" />
       </Head>
 
-      <Navbar />
-      <div
-        className={`${
-          showBackgroundImage ? "md:mt-[100px]" : ""
-        } mt-[65px] bg-contain pb-10 pt-10 md:pb-20 md:pt-20`}
-        style={{
-          backgroundImage: showBackgroundImage
-            ? "url('partner-bg.png')"
-            : "url('none')",
-          backgroundSize: "100% 100%",
-        }}
-      >
-        <div className="mx-auto flex max-w-[340px] flex-col items-center rounded-xl bg-white p-8 shadow-xl sm:max-w-[600px] md:max-w-[720px] lg:max-w-[960px]">
-          <div className="mb-8 text-center text-2xl md:text-3xl">
-            In collaboration with:
-          </div>
-          <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-2 md:gap-12 lg:grid-cols-3 xl:grid-cols-3">
-            {partners.map((partner, index) => (
-              <div key={index} className="rounded-md">
-                <img src={partner.image} alt="Partners" className="h-32 w-60 object-contain" />
-              </div>
-            ))}
-          </div>
-          <div className="mb-8 mt-12 text-center text-2xl md:mb-6 md:mt-2 md:text-3xl">
-            Supported by:
-          </div>
-          <div className="items-center justify-center md:flex">
-            <img
-              src="city-of-iloilo.jpeg"
-              className="mb-10 h-36 w-56 object-contain md:mb-0"
-              alt="City-Of-Iloilo"
-            />
-            <img src="doe-iloilo.jpeg" className="h-36 w-56 object-contain" alt="Doe-Iloilo"/>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <div
+          className={`${
+            showBackgroundImage ? "md:mt-[100px]" : ""
+          } mt-[65px] flex-grow bg-contain pb-10 pt-10 md:pb-20 md:pt-20`}
+          style={{
+            backgroundImage: showBackgroundImage
+              ? "url('partner-bg.png')"
+              : "none",
+            backgroundSize: "100% 100%",
+          }}
+        >
+          <div className="mx-auto flex max-w-[340px] flex-col items-center rounded-xl bg-white p-8 shadow-xl sm:max-w-[600px] md:max-w-[720px] lg:max-w-[960px]">
+            <div className="mb-8 text-center text-2xl md:text-3xl">
+              In collaboration with:
+            </div>
+            <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-2 md:gap-12 lg:grid-cols-3 xl:grid-cols-3">
+              {partners.map((partner, index) => (
+                <div key={index} className="rounded-md">
+                  <img
+                    src={partner.image}
+                    alt="Partners"
+                    className="h-32 w-60 object-contain"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="mb-8 mt-12 text-center text-2xl md:mb-6 md:mt-2 md:text-3xl">
+              Supported by:
+            </div>
+            <div className="items-center justify-center md:flex">
+              <img
+                src="city-of-iloilo.jpeg"
+                className="mb-10 h-36 w-56 object-contain md:mb-0"
+                alt="City-Of-Iloilo"
+              />
+              <img
+                src="doe-iloilo.jpeg"
+                className="h-36 w-56 object-contain"
+                alt="Doe-Iloilo"
+              />
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 };
