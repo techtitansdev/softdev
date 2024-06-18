5a46daece70222d71ed6ff192ecb40d3bed78e5c
Yoshifumi Suzuki <yoshifumisuzuki81194@gmail.com>
2024-06-18 19:25:34 +0800

    fix: bug fixes from recent push

--- src/server/api/routers/funding.ts
+++ src/server/api/routers/funding.ts
@@ -12,6 +12,7 @@ export const funding = createTRPCRouter({
         fundraiserId: z.string(),
         amount: z.number(),
         paymentMethod: z.string(),
+        donatedAs: z.string(),
       }),
     )
     .mutation(async (opts) => {
@@ -25,6 +26,7 @@ export const funding = createTRPCRouter({
           fundraiserId: input.fundraiserId,
           amount: input.amount,
           paymentMethod: input.paymentMethod,
+          donatedAs: input.donatedAs,
         },
       });
       return donor;
@@ -71,7 +73,7 @@ export const funding = createTRPCRouter({
         });
 
         return {
-          fullName: funding.fullName, // Change here
+          fullName: funding.fullName, 
           email: funding.email,
           contact: funding.contact,
           date: funding.date,
