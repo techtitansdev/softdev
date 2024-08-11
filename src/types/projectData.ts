4334804617123d128c202d9d14827d026b90beed
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-08-12 02:12:09 +0800

    fix(create-about): fix create about type

--- src/types/projectData.ts
+++ src/types/projectData.ts
@@ -6,21 +6,23 @@ export interface ProjectData {
   category: string;
   type: string;
   beneficiaries: string;
-  about: { // Change this to match the ProjectAboutData type
-    projectTitle: string,
-    projectDescription: string,
-    projectLink: string,
-    projectImage: string,
-    projectObjDescription: string,
-    projectObjImage: string,
-    projectName1: string,
-    projectName1Description: string,
-    projectName1Image: string,
-    projectName2: string,
-    projectName2Description: string,
-    projectName2Image: string,
-    theme: string,
-  },
+  about: ProjectAboutData;
   published: boolean;
   featured: boolean;
+}
+
+export interface ProjectAboutData {
+  projectTitle: string;
+  projectDescription: string;
+  projectLink: string;
+  projectImage: string;
+  projectObjDescription: string;
+  projectObjImage: string;
+  projectName1: string;
+  projectName1Description: string;
+  projectName1Image: string;
+  projectName2: string;
+  projectName2Description: string;
+  projectName2Image: string;
+  theme: string;
 }
