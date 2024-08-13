1290828ba5bf49e50b1bba8ec952470e4975cd10
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-08-13 22:04:39 +0800

    fix(blog-margin): fix the blog margin button

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -45,7 +45,7 @@ const Blogs = () => {
             </div>
           </div>
 
-          <div className="mb-12 mt-1 flex items-center justify-center">
+          <div className="mb-4 mt-1 flex items-center justify-center md:mb-12">
             <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {unfeaturedBlogs.map((blog: any) => (
                 <div key={blog.id}>
