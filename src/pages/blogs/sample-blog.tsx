5ebd4d193713fa8b8f55420c5d6efe2e2e5127e0
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-08-13 11:17:52 +0800

    feat(blog): added sample blog about ui

---
+++ src/pages/blogs/sample-blog.tsx
@@ -0,0 +1,86 @@
+import Head from "next/head";
+import { Footer } from "~/components/Footer";
+import { Navbar } from "~/components/Navbar";
+import Image from "next/image";
+
+const SampleBlogData = {
+  blogTitle: "Annual Curator's Meeting 2023",
+  blogDescription:
+    "Annual Curator’s Meeting 2023 of Global Shapers at the World Economic Forum in Geneva, Switzerland.",
+  blogImage: "/blog1.jpg",
+  blogDescription1:
+    "It was indeed an incredible experience to have met more than 500 shapers across 145 countries and learn about how they are each making a difference in their own hubs...",
+  blogImage1: "/blog3.jpg",
+  blogDescription2:
+    "Of course, this experience wouldn’t be complete without my crazy moments with Mark, Dan Dan, Jason Occi and our Community Champion Henna across PH hubs who are all inspiring and passionate!...",
+  blogImage2: "/blog2.jpg",
+};
+
+const SampleBlog = () => {
+  return (
+    <>
+      <div>
+        <Head>
+          <title> Blog | Global Shapers </title>
+          <meta name="description" content={SampleBlogData.blogDescription} />
+          <link rel="icon" href="/gsi-logo.png" />
+        </Head>
+
+        <Navbar />
+
+        <div className="mx-auto lg:max-w-[1200px]">
+          <div className="mb-4 flex items-center px-2 pt-20 text-2xl font-medium md:pt-32 md:text-3xl lg:max-w-[980px] lg:text-4xl">
+            {SampleBlogData.blogDescription}
+          </div>
+
+          <Image
+            src={SampleBlogData.blogImage}
+            alt="blog-image"
+            width={1200}
+            height={200}
+            className="px-2"
+          />
+
+          <div className="flex flex-col-reverse items-center px-2 pt-4 lg:flex-row lg:pt-16">
+            <div className="w-full flex-col items-center justify-center lg:w-1/2">
+              <Image
+                src={SampleBlogData.blogImage1}
+                alt="blog-image1"
+                className="mx-auto w-full"
+                width={600}
+                height={300}
+              />
+            </div>
+
+            <div className="mt-2 w-full rounded-md  lg:w-1/2 lg:max-w-[650px] lg:pr-6 xl:ml-12 xl:px-0">
+              <p className={` mb-4 text-sm sm:mb-8 sm:mt-8 md:text-lg`}>
+                {SampleBlogData.blogDescription1}
+              </p>
+            </div>
+          </div>
+
+          <div className="mb-12 flex flex-col items-center px-2 pt-4 lg:flex-row lg:pt-16">
+            <div className="w-full flex-col items-center justify-center lg:w-1/2">
+              <p className={` mb-4 text-sm sm:mb-8 sm:mt-8 md:text-lg`}>
+                {SampleBlogData.blogDescription2}
+              </p>
+            </div>
+
+            <div className="mt-2 w-full rounded-md  lg:w-1/2 lg:max-w-[650px] lg:pr-6 xl:ml-12 xl:px-0">
+              <Image
+                src={SampleBlogData.blogImage2}
+                alt="blog-image2"
+                className="mx-auto w-full"
+                width={600}
+                height={300}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default SampleBlog;
