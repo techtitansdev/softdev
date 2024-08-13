9a3ce9406f67080850c4e40817b2ec3711ffa503
Steven Ballaret <135417330+SteveBllrt@users.noreply.github.com>
2024-08-13 22:24:32 +0800

    refactor(fundraiser-view): fix fundraiser ui

--- src/pages/funded-projects/[id]/index.tsx
+++ src/pages/funded-projects/[id]/index.tsx
@@ -158,7 +158,7 @@ const Funding: React.FC = () => {
         </div>
 
         <div className="flex flex-col">
-          <div className="ml-10 space-x-4 text-sm font-medium sm:space-x-16 md:ml-24 md:space-x-24 md:text-base lg:space-x-32">
+          <div className="ml-10 space-x-8 text-sm font-medium sm:space-x-16 md:ml-24 md:space-x-24 md:text-base lg:space-x-32">
             <button
               onClick={() => changeContent("about")}
               className={`font-semibold text-gray-800 ${
@@ -192,9 +192,9 @@ const Funding: React.FC = () => {
           </div>
         </div>
 
-        <hr className="mx-6 my-4 h-px border-0 bg-gray-700 py-0.5 sm:mx-10"></hr>
+        <hr className="mx-5 my-4 h-px border-0 bg-gray-700 py-0.5 sm:mx-10"></hr>
 
-        <div className="mx-6 mb-12 mt-6 sm:mx-10 lg:mx-20 lg:mt-12">
+        <div className="mx-3 mb-12 mt-6 sm:mx-4 lg:mx-20 lg:mt-12">
           {content === "about" && fundingData?.project && (
             <SpecificAboutComponent />
           )}
